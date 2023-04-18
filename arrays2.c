aba184e481c651f5d6a58e0a9b655c35fa5aef71
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-04-18 06:25:06 +0530

    2D array in C

---
+++ arrays2.c
@@ -0,0 +1,13 @@
+//2D ARRAY : bunch of 1d's
+#include<stdio.h>
+int main()
+{
+    int arr[4][3]={{1, 4, 3},{3, 4, 1},{4, 5, 7},{1, 2, 3}};
+
+    for(int i=0; i<10; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
