4555fc1f62f0329efecd03d7f9647224b7a8bc8a
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-04-24 23:59:35 +0530

    arrays not valid

---
+++ arrays9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n, arr[n];
+    
+    return 0;
+
+}
+
+/*
+Array = {1, 2, 3, 4, 5}
+       1)      C doesn't allow declaring an array whose number of elements is not known at the complile time 
+ Therefore, the following array declarations are illegal in C 
+    int arr[]; //Wrong 
+    int n, arr[n]; //Wrong 
+
+*/
